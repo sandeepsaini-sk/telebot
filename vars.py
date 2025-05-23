d4ec2c8b0511cf8d386f97fdc8fa0598321f0341
sandeep saini <sandeepsaini21082003.email@example.com>
2025-05-23 08:54:56 +0530

    Initial commit

---
+++ vars.py
@@ -0,0 +1,12 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "20098112 "))
+API_HASH = environ.get("API_HASH", "5698e59220d0eb0162fe04854644181 ")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+OWNER = int(environ.get("OWNER", "6109886284"))
+CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
